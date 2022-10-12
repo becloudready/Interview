15654ce9dfe061daaffc6da2833b4e556ad44e40
Kunal Mehta <55488271+KunalMehta205@users.noreply.github.com>
2022-10-13 01:46:29 +0530

    Update question2.py

--- Python-Developer/question2.py
+++ Python-Developer/question2.py
@@ -5,7 +5,25 @@ def center_zeros(array):
     # write your function here
     # center means the floor(x / 2) where floor means rounding a float (decimal number) down to the nearest integer
     # i.e. floor(1) = 1, floor(1.5) = 1, floor(1.75) = 1, floor(2) = 2
-    pass
+    count_zero = 0
+    for i in array:
+        if i==0:
+            count_zero+=1
+
+    mid = floor(int(len(array)/2))
+    li= []
+
+    for i in range(0,(mid-count_zero)+1):
+        if array[i]!=0:
+            li.append(array[i])
+
+    for i in range(count_zero):
+        li.append(0)
+
+    for i in range(mid-1,len(array)):
+        if array[i]!=0:
+            li.append(array[i])
+    return li
 
 
 if __name__ == "__main__":
